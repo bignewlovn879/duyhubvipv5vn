5e5b3b459de26429aee0f28182556ecb15379fb0
bignewlovn879 <157965291+bignewlovn879@users.noreply.github.com>
2024-02-03 20:00:25 +0700

    Update duyhubvipvn.lua

--- duyhubvipvn.lua
+++ duyhubvipvn.lua
@@ -1,3 +1,18 @@
+spawn(function() 
+    repeat
+        task.wait()
+    until game:IsLoaded()
+    repeat
+        task.wait()
+    until game.Players
+    repeat
+        task.wait()
+    until game.Players.LocalPlayer and game.Players.LocalPlayer.Team ~= nil 
+    wait(1.5)
+    require(game.ReplicatedStorage.Notification).new("<Color=Red> WELECOM TO VŨ DUY HUB<Color=/>"):Display()
+    require(game.ReplicatedStorage.Notification).new("<Color=Yellow>THÔNG BÁO:BẢN V5 ĐẦU TIÊN DUY HUB VN<Color=/>"):Display()
+end)
+----------------------------------------------------------------------------------------------------------------------------------------------
 local Notif = {}
 
 local CoreGUI = game:GetService("CoreGui")
